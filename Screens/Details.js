91983d029afd09ea5a3b665ecc306aed6c5856b6
Albert-Andrews <aaaquaye001@st.ug.edu.gh>
2024-07-11 17:45:48 +0000

    "Updated Details screen with new imports, navigation, and styles"

--- Screens/Details.js
+++ Screens/Details.js
@@ -1,13 +1,45 @@
-import { SafeAreaView, StyleSheet, View, Image } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React from "react";
 
-const Details = () => {
+const Details = ({navigation}) => {
   return (
-    <SafeAreaView>
-      <View>
-        <View>
-        <Text></Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.subContainer}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+            <Image style={styles.icon} source={require("../assets/Menu.png")} />
+          </TouchableOpacity>
+
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerText}>Open</Text>
+            <Text style={styles.headerTextSub}>Fashion</Text>
+          </View>
+          <View style={styles.iconContainer}>
+            <TouchableOpacity>
+              <Image
+                style={styles.icon}
+                source={require("../assets/icons8-search.png")}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Cart", { cartItems })}
+            >
+              <Image
+                style={styles.icon}
+                source={require("../assets/shopping-bag.png")}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
+       
+
       </View>
     </SafeAreaView>
   );
@@ -15,4 +47,37 @@ const Details = () => {
 
 export default Details;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  subContainer: {
+    padding: 10,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  headerTextContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  headerTextSub: {
+    fontSize: 24,
+    fontWeight: "300",
+    marginLeft: 4,
+  },
+  iconContainer: {
+    flexDirection: "row",
+    gap: 20,
+  },
+});
